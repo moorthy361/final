b183cf61bd9503437e836348eed8c92e59c5fd62
moorthy361 <moorthy.alliedminds@gmail.com>
2026-09-19 10:46:40 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -24,7 +24,7 @@ class Settings(BaseSettings):
         "http://localhost:3000",
         "http://127.0.0.1:3000",
          # Production frontend
-    "https://final-n9z57hjbc-moorthy361.vercel.app",
+    "https://final-five-navy-25.vercel.app",
     ]
 
     # Auto-seed database
